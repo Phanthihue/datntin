cc09e28ab496c46faf039e3b8ebe48460363b297
phanhue <phanhue9080@gmail.com>
2019-12-05 00:05:48 +0700

    h1

--- Datn/side/src/app/app-routing.module.ts
+++ Datn/side/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'dashboard-tabs',
+    loadChildren: () => import('./dashboard-tabs/dashboard-tabs.module').then( m => m.DashboardTabsPageModule)
   }
 ];
 
--- Datn/side/src/app/home/home.page.ts
+++ Datn/side/src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Platform} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage 
+{
+  selectedval:Number=102;
+  data:any[]=[]; 
+  constructor(private platform:Platform) 
+  {
+  	this.platform.ready().then(()=>
+  	{
+  		this.data=[{id:101,name:"name1"},{id:102, name:"name2"}];
+  	})
+  }
+  	onchange(event)
+  	{
+  		 alert("you have select id = "+ event.target.value);
+  	}
 }
